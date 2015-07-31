36df8c830d37829ceaac1ab343ea4ac98fb7ea90
CarterBi <carterbian32@gmail.com>
2015-07-31 11:23:03 -0700

    FINAL Update Multiplication

--- Multiplication.py
+++ Multiplication.py
@@ -1,6 +1,4 @@
 
-num = int(raw_input("What number do you want to go up to? \nPlease do not exceed 30."))
-
 while True:
 	num = int(raw_input("What number do you want to go up to? \nPlease do not exceed 30."))
 	if num > 30:
