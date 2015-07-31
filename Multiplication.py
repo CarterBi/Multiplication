eb28fc32b2cfd3b0a3cf67ec73a7e3b9347c21a8
CarterBi <carterbian32@gmail.com>
2015-07-31 11:16:08 -0700

    Updated Multiplication

--- Multiplication.py
+++ Multiplication.py
@@ -1,13 +1,14 @@
+
 num = int(raw_input("What number do you want to go up to? \nPlease do not exceed 30."))
 
-num2 = range(1,num + 1)
-
 while True:
-
+	num = int(raw_input("What number do you want to go up to? \nPlease do not exceed 30."))
 	if num > 30:
 		print "Please write a smaller number."
 	else:
 		break
+
+num2 = range(1,num + 1)
 
 for i in num2:
 	for j in num2:
